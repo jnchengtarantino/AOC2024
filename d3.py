650de6d7b55781d80a655b0efdbb3e1d766e1ca3
JN <jn@chengtarantino.com>
2024-12-03 07:30:22 -0500

    d3

--- d3.py
+++ d3.py
@@ -5,14 +5,26 @@ from itertools import accumulate, groupby
 from math import lcm
 import re
 
+patt = r"mul\((\d{1,3}),(\d{1,3})\)"
+patt2 = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+
 def part1(lines):
-    pass
+    return sum([int(x)*int(y) for (x,y) in re.findall(patt, parseInput(lines))])
 
 def part2(lines):
-    pass
+    dat = re.findall(patt2, parseInput(lines))
+    do = True
+    res = 0
+    for d in dat:
+        if d == "do()": do = True
+        elif d == "don't()" : do = False
+        elif do:
+            m = re.match(patt, d)
+            res = res + (int(m.group(1)) * int(m.group(2)))
+    return res
     
 def parseInput(lines):
-    pass
+    return ''.join([line for line in lines])
 
 if __name__ == "__main__":
     testFile = open("test.txt")
